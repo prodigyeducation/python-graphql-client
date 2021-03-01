4de0ad0ef9e3bdc20b9248eb3f7b461372d8ffc8
Dale Seo <dale.seo@gmail.com>
2021-02-28 19:46:17 -0500

    stop using root logger

--- python_graphql_client/graphql_client.py
+++ python_graphql_client/graphql_client.py
@@ -13,6 +13,7 @@ class GraphqlClient:
 
     def __init__(self, endpoint: str, headers: dict = {}, **kwargs: Any):
         """Insantiate the client."""
+        self.logger = logging.getLogger(__name__)
         self.endpoint = endpoint
         self.headers = headers
         self.options = kwargs
@@ -104,8 +105,8 @@ class GraphqlClient:
             async for response_message in websocket:
                 response_body = json.loads(response_message)
                 if response_body["type"] == "connection_ack":
-                    logging.info("the server accepted the connection")
+                    self.logger.info("the server accepted the connection")
                 elif response_body["type"] == "ka":
-                    logging.info("the server sent a keep alive message")
+                    self.logger.info("the server sent a keep alive message")
                 else:
                     handle(response_body["payload"])
